6bc5f68c5e3d2f3590c482463f703545f4fc940a
monicas <monica.s@gdn-commerce.com>
2024-12-18 16:37:14 +0530

    Changes

--- src/main/java/com/example/repomindmap/RepoMindMapGenerator.java
+++ src/main/java/com/example/repomindmap/RepoMindMapGenerator.java
@@ -6,12 +6,15 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 
 import java.io.File;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class RepoMindMapGenerator {
+
+    private static final String DOT = ".";
 
     public static Map<String, ClassOrInterfaceNode> generateMindMap(File directory) {
         Map<String, ClassOrInterfaceNode> mindMap = new LinkedHashMap<>();
@@ -34,72 +37,78 @@ public class RepoMindMapGenerator {
         try {
             JavaParser javaParser = new JavaParser();
             CompilationUnit cu = javaParser.parse(file).getResult().orElseThrow(() -> new RuntimeException("Failed to parse Java file"));
-            String packageName = cu.getPackageDeclaration().map(pd -> pd.getNameAsString()).orElse("No Package");
             cu.findAll(ClassOrInterfaceDeclaration.class).forEach(unit -> {
-                String name = unit.getNameAsString();
-                ClassOrInterfaceNode node = ClassOrInterfaceNode.builder().packageName(packageName).name(name).build();
-                List<ClassOrInterfaceNode> interfaces = new ArrayList<>();
-                unit.getExtendedTypes().stream().findFirst().ifPresent(interfaceType -> {
-                    ClassOrInterfaceNode interfaceNode = new ClassOrInterfaceNode();
-                    interfaceNode.setName(interfaceType.getNameAsString());
-                    interfaceNode.setPackageName(interfaceType.getMetaModel().getPackageName());
-                    //TODO: Recurrion approach for parent data
-                    interfaces.add(interfaceNode);
-                });
-
-                unit.getImplementedTypes().forEach(parent -> {
-                    node.setContainsParent(true);
-                    ClassOrInterfaceNode parentNode = new ClassOrInterfaceNode();
-                    parentNode.setName(parent.getNameAsString());
-                    parentNode.setContainsParent(parent.hasParentNode());
-                    parentNode.setPackageName(parent.getMetaModel().getPackageName());
-                    if (parentNode.isContainsParent()) {
-                        //TODO: Recursion approach for multi hierarchy
-                        //parentNode.setParentNode(parent.getParentNode());
-                    }
-                    node.setParentNode(parentNode); // Set parent node here
-                });
-                if(!interfaces.isEmpty()) {
-                    node.setUsesInterface(true);
-                    node.setInterfaceNodeList(interfaces);
-                }
-                // Find methods
-                List<MethodNode> methods = new ArrayList<>();
-                unit.getMethods().forEach(method -> {
-                    MethodNode methodNode = MethodNode.builder()
-                            .name(method.getNameAsString())  // Set the method name
-                            .returnType(method.getType().asString())  // Set the return type
-                            .parameterTypes(method.getParameters().stream()
-                                    .map(parameter -> parameter.getType().asString()) // Get the type of each parameter
-                                    .collect(Collectors.toList()))  // Collect them into a list
-                            .exceptionTypes(method.getThrownExceptions().stream()
-                                    .map(exception -> {
-                                        try {
-                                            return Class.forName(exception.getClass().getName());  // Convert exception name to Class type
-                                        } catch (ClassNotFoundException e) {
-                                            e.printStackTrace();
-                                            return null;
-                                        }
-                                    })
-                                    .toArray(Class<?>[]::new))
-                            .modifiers(method.getModifiers().stream()
-                                    .map(modifier -> modifier.getKeyword().asString())
-                                    .collect(Collectors.toList()))
-                            .signature(method.getDeclarationAsString())  // Get the method signature (name + parameters + return type)
-                            .annotations(method.getAnnotations().stream().map(annotationExpr -> annotationExpr.getName().asString()).collect(Collectors.toList()))
-                            .body(method.getBody().map(BlockStmt::toString).orElse(""))
-                            .build();
-                    System.out.println(methodNode);
-                    methods.add(methodNode);
-                });
-                node.setMethodList(methods);
-                //TODO: Figure out on memebers & methods
-                node.setModifiers(unit.getModifiers().stream().map(modifier -> modifier.getKeyword().asString()).collect(Collectors.toList()));
-                node.setAnnotations(unit.getAnnotations().stream().map(annotationExpr -> annotationExpr.getName().asString()).collect(Collectors.toList()));
-                classInfoMap.put(packageName + name, node);
+                ClassOrInterfaceNode node = getClassOrInterfaceNode(unit);
+                classInfoMap.put(node.getNodeKey(), node);
             });
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static ClassOrInterfaceNode getClassOrInterfaceNode(ClassOrInterfaceDeclaration unit) {
+        String packageName = unit.getMetaModel().getPackageName();
+        String name = unit.getNameAsString();
+        ClassOrInterfaceNode node = ClassOrInterfaceNode.builder().packageName(packageName).name(name).nodeKey(packageName+ DOT +name).build();
+        List<ClassOrInterfaceNode> extendNodes = new ArrayList<>();
+        unit.getExtendedTypes().stream().findFirst().ifPresent(extendedNode -> {
+            ClassOrInterfaceNode extendNode = getRelatedNodes(extendedNode);
+            //TODO: Recurrion approach for parent data
+            extendNodes.add(extendNode);
+        });
+        node.setExtendsNode(extendNodes);
+        List<ClassOrInterfaceNode>  implementNodes = new ArrayList<>();
+        unit.getImplementedTypes().forEach(parent -> {
+            ClassOrInterfaceNode implementNode = getRelatedNodes(parent);
+            //TODO: Recursion approach for multi hierarchy
+            implementNodes.add(implementNode);
+        });
+        node.setImplementsNode(implementNodes);
+        // Find methods
+        List<MethodNode> methods = new ArrayList<>();
+        methodExtractor(unit, methods);
+        node.setMethodList(methods);
+        //TODO: Figure out on memebers & methods
+        node.setModifiers(unit.getModifiers().stream().map(modifier -> modifier.getKeyword().asString()).collect(Collectors.toList()));
+        node.setAnnotations(unit.getAnnotations().stream().map(annotationExpr -> annotationExpr.getName().asString()).collect(Collectors.toList()));
+        return node;
+    }
+
+    private static ClassOrInterfaceNode getRelatedNodes(ClassOrInterfaceType extendedNode) {
+        ClassOrInterfaceNode extendNode = new ClassOrInterfaceNode();
+        extendNode.setName(extendedNode.getNameAsString());
+        extendNode.setPackageName(extendedNode.getMetaModel().getPackageName());
+        extendNode.setNodeKey(extendNode.getPackageName()+DOT+extendNode.getName());
+        return extendNode;
+    }
+
+    private static void methodExtractor(ClassOrInterfaceDeclaration unit, List<MethodNode> methods) {
+        unit.getMethods().forEach(method -> {
+            MethodNode methodNode = MethodNode.builder()
+                    .name(method.getNameAsString())  // Set the method name
+                    .returnType(method.getType().asString())  // Set the return type
+                    .parameterTypes(method.getParameters().stream()
+                            .map(parameter -> parameter.getType().asString()) // Get the type of each parameter
+                            .collect(Collectors.toList()))  // Collect them into a list
+                    .exceptionTypes(method.getThrownExceptions().stream()
+                            .map(exception -> {
+                                try {
+                                    return Class.forName(exception.getClass().getName());  // Convert exception name to Class type
+                                } catch (ClassNotFoundException e) {
+                                    e.printStackTrace();
+                                    return null;
+                                }
+                            }).distinct()
+                            .toArray(Class<?>[]::new))
+                    .modifiers(method.getModifiers().stream()
+                            .map(modifier -> modifier.getKeyword().asString())
+                            .collect(Collectors.toList()))
+                    .signature(method.getDeclarationAsString())  // Get the method signature (name + parameters + return type)
+                    .annotations(method.getAnnotations().stream().map(annotationExpr -> annotationExpr.getName().asString()).collect(Collectors.toList()))
+                    .body(method.getBody().map(BlockStmt::toString).orElse(""))
+                    .build();
+            System.out.println(methodNode);
+            methods.add(methodNode);
+        });
     }
 }
--- src/main/java/com/example/repomindmap/model/ClassOrInterfaceNode.java
+++ src/main/java/com/example/repomindmap/model/ClassOrInterfaceNode.java
@@ -8,8 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.lang.reflect.Member;
-import java.lang.reflect.Method;
 import java.util.List;
 
 
@@ -22,12 +20,11 @@ import java.util.List;
 public class ClassOrInterfaceNode {
   private String name;
   private String packageName;
-  private boolean isContainsParent;
+  private String nodeKey;
   private ClassOrInterfaceNode parentNode;
-  private boolean isUsesInterface;
-  private List<ClassOrInterfaceNode> interfaceNodeList;
+  private List<ClassOrInterfaceNode> implementsNode;
+  private List<ClassOrInterfaceNode> extendsNode;
   private List<MethodNode> methodList;
   private List<String> annotations;
-  private List<String> members;
   private List<String> modifiers;
 }
